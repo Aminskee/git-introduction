44fe90aa0da12c7e934cc9a0b90b73473ec186a2
Amine Mastouri <mstouri@me.com>
2024-04-21 18:29:22 +0200

    [1]- add welcome to dubai

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void welcome(){
+        System.out.println("Hello world!");
+        System.out.println("Welcome to Dubai!");
+    }
 }
